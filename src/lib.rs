142ae1d24d208e0c2543397ed3cef63500ee91a4
ahaoboy <504595380@qq.com>
2025-12-03 15:52:42 +0800

    ci

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,18 @@ use pyo3::prelude::*;
 #[pymodule]
 mod ci_test {
     use pyo3::prelude::*;
+    use rquickjs_core::{Context,   Result, Runtime };
 
     /// Formats the sum of two numbers as string.
     #[pyfunction]
     fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+        let rt = Runtime::new().unwrap();
+        rt.set_memory_limit(u32::MAX as usize);
+        rt.set_max_stack_size(u32::MAX as usize);
+        let ctx = Context::full(&rt).unwrap();
+        ctx.with(|ctx| -> Result<()> {
+            ctx.eval("1+1")?
+        }).unwrap();
         Ok((a + b).to_string())
     }
 }
